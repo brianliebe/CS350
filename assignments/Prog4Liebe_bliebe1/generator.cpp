7cbb034e799e62082746acc1dd26721977548609
bliebe1@binghamton.edu <brianliebe@yahoo.com>
2018-03-14 20:58:18 -0400

    Added comments to generator

--- assignments/Prog4Liebe_bliebe1/generator.cpp
+++ assignments/Prog4Liebe_bliebe1/generator.cpp
@@ -21,16 +21,19 @@ int main(int argc, char **argv) {
 	srand(time(NULL));
 	
 	if (type == 0) {
+		// No locality, so generate N integers between 0 and # of pages
 		for (int i = 0; i < num_access; i++) {
 			output << rand() % num_pages << endl;
 		}
 	}
 	else if (type == 1) {
-		vector<int> often;
-		vector<int> scarce;
+		vector<int> often; // the 20% of pages which are often used
+		vector<int> scarce; // the 80% of pages which are scarcely used
 
+		// take 20% of of the numbers 0 - #pages and set them as often used
 		for (int i = 0; i < num_pages / 5; i++) {
 			int value = rand() % num_pages;
+			// Looks to see if the value is already selected
 			bool found = false;
 			for (unsigned int j = 0; j < often.size(); j++) {
 				if (often[j] == value) {
@@ -38,14 +41,17 @@ int main(int argc, char **argv) {
 					break;
 				}
 			}
-			if (!found) {
+			if (!found) { // if is new, push it
 				often.push_back(value);
 			}
-			else {
+			else { // if it's already there, we need to generate a new number
 				i--;
 			}
 		}
+
+		// take the remaining numbers and set them as scarcely used
 		for (int i = 0; i < num_pages; i++) {
+			// see if the value is already set as often used
 			bool found = false;
 			for (unsigned int j = 0; j < often.size(); j++) {
 				if (often[j] == i) {
@@ -53,23 +59,32 @@ int main(int argc, char **argv) {
 					break;
 				}
 			}
-			if (!found) {
+			if (!found) { // if it's not often used, push it
 				scarce.push_back(i);
 			}
 		}
+		
+		// vector to hold all N accesses
 		vector<int> accesses;
+
+		// add 80% of N from the often used vector
 		for (unsigned int i = 0; i < (num_access*4)/5; i++) {
 			accesses.push_back(often[rand() % often.size()]);
 		}
+
+		// add the remaining 20% of N from the scarcely used vector 
 		for (unsigned int i = 0; i < num_access/5; i++) {
 			accesses.push_back(scarce[rand() % scarce.size()]);
 		}
+
+		// shuffle the data so that it's distributed normally, then add it to the file
 		random_shuffle(accesses.begin(), accesses.end());
 		for (int i = 0; i < num_access; i++) {
 			output << accesses[i] << endl;
 		}
 	}
 	else if (type == 2) {
+		// just loop from 0 - #pages, then start from 0 again
 		int value = 0;
 		for (int i = 0; i < num_access; i++) {
 			output << value << endl;
@@ -77,6 +92,7 @@ int main(int argc, char **argv) {
 			if (value == num_pages) value = 0;
 		}
 	}
+	// close output
 	output.close();
 }
 
