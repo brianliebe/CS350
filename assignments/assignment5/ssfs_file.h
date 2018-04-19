865fe2942eb7cc0f8077e4a395f38aada7f26d35
Brian Liebe <bliebe1@cs.binghamton.edu>
2018-04-19 14:29:18 -0400

    I hate this

--- assignments/assignment5/ssfs_file.h
+++ assignments/assignment5/ssfs_file.h
@@ -2,21 +2,29 @@
 #define SSFS_FILE_H
 
 typedef struct indirect_block {
-	int d_size;
-	std::vector<int> block_numbers;
+	int **direct_blocks;
 } indirect_block;
 
 typedef struct double_indirect_block {
-	int d_size;
-	std::vector<int> block_numbers;
+	int **indirect_blocks;
 } double_indirect_block;
 
 typedef struct inode {
 	std::string file_name;
 	int file_size;
 	int direct_block_pointers[12];
-	indirect_block *indirect_block_pointer;
-	double_indirect_block *double_indirect_block_pointer;	
+	int indirect_block_pointer;
+	int double_indirect_block_pointer;	
 } inode;
+
+typedef struct inode_map {
+	// each entry is 36 bytes
+	std::string *file_names;
+	int** inode_locations;
+} inode_map;
+
+typedef struct free_block_list {
+	int **free_blocks;
+} free_block_list;
 
 #endif
--- assignments/assignment5/ssfs_mkdsk.cpp
+++ assignments/assignment5/ssfs_mkdsk.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <fstream>
 #include "ssfs_file.h"
@@ -34,5 +35,46 @@ int main (int argc, char **argv)
 	disk_file.seekp((num_blocks * block_size) - 1);
 	disk_file.write("", 1);
 
+	// write inode map to file
+	disk_file.seekp(0);
+	int max_entries = 256;
+	printf("%d\n", max_entries);
+
+	for (int i = 0; i < max_entries; i++)
+	{
+		char temp_file[32];
+		strcpy(temp_file, "NULL_FILE");
+		disk_file.write((char*)&temp_file, sizeof(temp_file));
+	}
+	for (int i = 0; i < max_entries; i++)
+	{
+		int temp_int = -1;
+		disk_file.write((char*)&temp_int, sizeof(temp_int));
+	}
+
+	// write free block list to file
+	disk_file.seekp(72 * block_size);
+	int blocks_in_use = ((num_blocks) * 4) / block_size;
+	printf("%d\n", blocks_in_use);
+
+	for (int i = blocks_in_use + 72; i < num_blocks; i++) 
+	{
+		// start at the first block after the free block list
+		disk_file.write((char*)&i, sizeof(i));
+	}
+	int final_entry = -2;
+	disk_file.write((char*)&final_entry, sizeof(final_entry));
+
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
